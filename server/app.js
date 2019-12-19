bd5e52a5ce032ebdf1c3680dae92912788fcad95
RandiBierbaum <randibierbaum@gmail.com>
2019-12-19 10:00:45 +0100

    mongoose.connect

--- server/app.js
+++ server/app.js
@@ -120,8 +120,8 @@ const categoryDAL = require('./dal/category_dal')(mongoose);
 const userDAL = require('./dal/user_dal')(mongoose);
 
 /**** Start ****/
-const MONGO_URL = process.env.MONGO_URL || 'mongodb+srv://randi:Sommerfugl89@cluster0-retun.mongodb.net/test?retryWrites=true&w=majority';
-//const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost/secondhand-book-store';
+//const MONGO_URL = process.env.MONGO_URL || 'mongodb+srv://randi:Sommerfugl89@cluster0-retun.mongodb.net/test?retryWrites=true&w=majority';
+const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost/secondhand-book-store';
 mongoose.connect(MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(async () => {
         console.log("Database connected");
